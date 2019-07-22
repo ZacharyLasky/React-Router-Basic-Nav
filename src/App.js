74a7a71fe0aa50eedb68e8b8105ac2b01057ec4c
Zach Lasky <zacharylasky123@gmail.com>
2019-07-22 17:00:07 -0400

    created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" exact component={index} />
+    <Route path="/about/" component={about} />
+    <Route path="/contact/" component={index} />
   </div>
 );
 
